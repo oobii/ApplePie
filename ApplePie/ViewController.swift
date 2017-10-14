abd158cb569ef3618d76905bdf54bbaebee01edd
martynov <vvayfarer@yandex.com>
2017-10-13 22:56:14 -0400

    Word-guessing game, first commit -  interface only

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    @IBOutlet var letterButtons: [UIButton]!
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        sender.isEnabled = false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
